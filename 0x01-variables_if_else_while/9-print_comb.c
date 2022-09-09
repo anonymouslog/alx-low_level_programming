8046deb3f214a19fa575c0605f8938b398805525
Tobi <bismarktobi@yahoo.com>
2022-09-09 16:54:46 -0500

    all task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,16 +11,16 @@ int c;
 
 for (c = '0'; c <= '9'; c++)
 {
-    putchar(c);
+putchar(c);
 
-        if (c != '9')
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
+if (c != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
+return (0);
 }
