868cab24487b496d078a1f0a36a56383055a537c
Tobi <bismarktobi@yahoo.com>
2022-09-09 16:53:09 -0500

    all task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,19 +7,19 @@
   */
 int main(void)
 {
-    char c;
+char c;
 
-    for (c = '0'; c <= '9'; c++)
-    {
-        putchar(c);
-    }
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
 
-    for (c = 'a'; c <= 'f'; c++)
-    {
-        putchar(c);
-    }
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
+return (0);
 }
