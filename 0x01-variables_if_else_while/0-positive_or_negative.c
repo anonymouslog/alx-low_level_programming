b925ed4ec19374968aec36b57e08b02a5b6fe697
Tobi <bismarktobi@yahoo.com>
2022-09-11 11:33:09 -0500

    all task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,13 +7,12 @@
  *
  * Return: Always (Success)
  */
+
 int main(void)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
 if (n > 0)
 {
 printf("%d is positive\n", n);
@@ -26,6 +25,5 @@ else
 {
 printf("%d is negative\n", n);
 }
-
 return (0);
 }
