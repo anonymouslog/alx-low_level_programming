cc420a88a052712934c6a39e40bf3f6fe2de318e
Tobi <bismarktobi@yahoo.com>
2022-09-14 06:31:00 -0500

    resolved

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /** 
- * main - prints putchar
- * Return: always 0
- */
-
+* main - prints putchar
+* Return: always 0
+*/
 int main(void)
+
 {
 _putchar('_');
 _putchar('p');
