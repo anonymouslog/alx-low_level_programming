d323b18558ea88b3fd34df42d16f5d89043ad114
Tobi <anonymouslog123@gmail.com>
2022-10-04 10:12:06 +0100

    0 create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,12 +1,24 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
+ * create_array - creates an array of chars, and
+ * initializes it with a specific char.
+ * @size: size of the array
+ * @c: character to insert
+ * Return: NULL if size is zero or if it fails.
+ * pointer to array if everything is normal.
  */
-int _putchar(char c)
+
+char *create_array(unsigned int size, char c)
 {
-return (write(1, &c, 1));
+char *array;
+unsigned int index;
+if (size == 0)
+return (NULL);
+array = malloc(sizeof(char) * size);
+if (array == NULL)
+return (NULL);
+for (index = 0; index < size; index++)
+array[index] = c;
+return (array);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
+
 #include <stdlib.h>
+
 int _putchar(char C);
 char *ctreate_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -9,4 +10,6 @@ char *str_contact(char *s1, char *s2);
 int **alloc_grid(int **grid, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
-#endif
+char **strtow(char *str);
+
+#endif /* MAIN_H */
