b7cc3fa53dfc406d8a9254af2fe9d2a85d316923
Tobi <bismarktobi@yahoo.com>
2022-10-05 15:46:50 +0100

    corrected main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,7 +7,7 @@ int _putchar(char C);
 char *ctreate_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_contact(char *s1, char *s2);
-int **alloc_grid(int **grid, int height);
+int **alloc_grid(int **width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
